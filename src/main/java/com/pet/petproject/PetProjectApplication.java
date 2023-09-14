a0422d13cd5d4a4c92a67a8a1068f9b57b679945
sungjin <sungjinny5@naver.com>
2023-09-14 20:18:47 +0900

    init

--- src/main/java/com/pet/petproject/PetProjectApplication.java
+++ src/main/java/com/pet/petproject/PetProjectApplication.java
@@ -8,6 +8,7 @@ public class PetProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PetProjectApplication.class, args);
+        System.out.println("hello world");
     }
 
 }
